f718eb343ef4240fda53f4eeace033f3e997a2b2
alexander <alexandr.larionov88@gmail.com>
2019-02-25 16:38:36 -0800

    comment delete and edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,37 @@
 class CommentsController < ApplicationController
     before_action :set_post
+    before_action :set_comment, only: [:destroy, :edit, :update]
+
     def create
         @post.comments << Comment.create(comments_params)
         redirect_to @post
+    end
+
+    def destroy
+        p params
+        @comment.destroy if @comment
+        redirect_to @post
+    end
+
+    def edit
+    end
+
+    def update
+        if @comment.update(comments_params)
+            redirect_to @post
+        else
+            render :edit
+        end
     end
 
     private
 
     def set_post
         @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+        @comment = Comment.find(params[:id])
     end
 
     def comments_params
