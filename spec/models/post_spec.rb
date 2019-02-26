dc6d3977ceba3314d8289406b1df55e57b7ff802
alexander <alexandr.larionov88@gmail.com>
2019-02-25 17:39:58 -0800

    rspec setup and Post tests

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Post" do
+  it "is valid with a title and body" do
+    post = Post.new(title: 'title', body: 'post body')
+    expect(post).to be_valid
+  end
+
+  it "is searches for posts" do
+    post1 = Post.create(title: 'hello world')
+    post2 = Post.create(title: 'coding challenge')
+    posts = Post.search('hello')
+
+    expect(posts.count).to eq(1)
+    expect(posts).to eq([post1])
+  end
+end
+
